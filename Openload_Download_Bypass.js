d77a0a4160d26c5e97c387d991249e72a1ca8ca2
Diegiwg <diegiwg@gmail.com>
2018-04-29 14:11:26 -0300

    Update Openload_Download_Bypass.js

--- Openload_Download_Bypass.js
+++ Openload_Download_Bypass.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Openload Download Bypass
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Openload Download Bypass [2018]
 // @icon         http://pics.smotri.com/cskins/blue/smiles/ai.gif
-// @downloadURL  https://gist.github.com/TwelveCharzz/393603140883a9672ec8/raw/Openload_download_bypass.user.js
-// @updateURL    https://gist.github.com/TwelveCharzz/393603140883a9672ec8/raw/Openload_download_bypass.user.js
+// @downloadURL  https://raw.githubusercontent.com/Diegiwg/Openload-Download-Bypass/master/Openload_Download_Bypass.js
+// @updateURL    https://raw.githubusercontent.com/Diegiwg/Openload-Download-Bypass/master/Openload_Download_Bypass.js
 // @author       Diegiwg
 // @match        http*://oload.win/f/*
 // @grant        GM_setClipboard
